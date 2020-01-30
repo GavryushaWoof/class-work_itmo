40960511639f56bf08f399a017858ddf3ffc3cc1
GavryushaWoof <gavgav.cat@yandex.ru>
2020-01-30 19:45:55 +0300

    30.01

---
+++ module.4/30.01/node.js/1.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+let file = fs.readFileSync('1.js', 'utf-8');
+
+console.log(file);
---
+++ module.4/30.01/node.js/2.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.readFile('app.js', 'utf-8', function(err, data) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data);
+    }
+});
+
+console.log('Все текущие команды JS выполнены');
